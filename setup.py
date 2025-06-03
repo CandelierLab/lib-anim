b57d575ea68d1b72a452d02180a6b6b019344996
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2025-06-03 17:25:07 +0200

    Push 1.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 DESCRIPTION = 'Beautifully simple animations'
 LONG_DESCRIPTION = 'A content-oriented package to create animations'
 
